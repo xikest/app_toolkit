2547f12afef3441668a72429f600cd103c7c977f
xikest <xikest12@gmail.com>
2023-12-24 21:48:16 +0900

    .

--- app.py
+++ app.py
@@ -22,60 +22,61 @@ def main():
         # st.subheader("YouTube Downloader")
         st.write("Private project")
         st.markdown("---")
-        # st.write("Written by TJ.Kim ☕")
+        st.write("Written by TJ.Kim ☕")
         st.markdown("---")
 
     # User input for YouTube URL
     url = st.text_input(label="Enter URL:")
     # st.write("Entered URL:", url)
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button("video, 720p, mp4"):
+            filename=None
+            for _ in range(10):
+                try:
+                    filename = download_video(url)
+                    break
+                except:
+                    pass
 
-    if st.button("video:max 720p)"):
-        filename=None
-        for _ in range(10):
-            try:
-                filename = download_video(url)
-                break
-            except:
-                pass
 
+            if filename is not None:
+                st.success(f"Video downloaded successfully: {filename}")
+                # Display download button
+                file_path = os.path.join(os.getcwd(), f"{filename}")
+                with open(file_path, "rb") as file:
+                    video_file = file.read()
+                    st.download_button(
+                        label="Download Video",
+                        data=video_file,
+                        file_name=f"{filename}",
+                        mime="video/mp4"
+                    )
+            else:
+                st.error("Failed to download video.", icon= "🚨")
+    with col2:
+        if st.button("audio, mp4"):
+            filename = None
+            for _ in range(10):
+                try:
+                    filename = download_audio(url)
+                    break
+                except:
+                    pass
 
-        if filename is not None:
-            st.success(f"Video downloaded successfully: {filename}")
-            # Display download button
-            file_path = os.path.join(os.getcwd(), f"{filename}")
-            with open(file_path, "rb") as file:
-                video_file = file.read()
-                st.download_button(
-                    label="Download Video",
-                    data=video_file,
-                    file_name=f"{filename}",
-                    mime="video/mp4"
-                )
-        else:
-            st.error("Failed to download video.")
-
-    if st.button("audio"):
-        filename = None
-        for _ in range(10):
-            try:
-                filename = download_audio(url)
-                break
-            except:
-                pass
-
-        if filename is not None:
-            st.success(f"Audio downloaded successfully: {filename}")
-            # Display download button
-            file_path = os.path.join(os.getcwd(), f"{filename}")
-            with open(file_path, "rb") as file:
-                video_file = file.read()
-                st.download_button(
-                    label="Download Audio",
-                    data=video_file,
-                    file_name=f"{filename}",
-                    mime="audio/mp4"
-                )
-        else:
-            st.error("Failed to download audio.")
+            if filename is not None:
+                st.success(f"Audio downloaded successfully: {filename}")
+                # Display download button
+                file_path = os.path.join(os.getcwd(), f"{filename}")
+                with open(file_path, "rb") as file:
+                    video_file = file.read()
+                    st.download_button(
+                        label="Download Audio",
+                        data=video_file,
+                        file_name=f"{filename}",
+                        mime="audio/mp4"
+                    )
+            else:
+                st.error("Failed to download audio.", icon= "🚨")
 if __name__ == "__main__":
     main()
